43dfc3a02a6cb26ed16a5747d88033d50e3819bf
Aqshola <aqsholafid2@gmail.com>
2020-11-16 15:58:38 +0700

    creating home display

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ app.use(
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res, next) => {
-  console.log(req.session);
-  if (!req.session.view) req.session.view = 0;
-  req.session.view++;
-  res.render("index", { view: req.session.view });
+app.get("/", (req, res) => {
+  res.render("pages/index");
 });
 app.listen(5000, console.log("server started"));
