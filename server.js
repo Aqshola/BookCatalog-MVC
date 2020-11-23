ebc43b084c50ab358ec65ace6f5046954535d938
Aqshola <aqsholafid2@gmail.com>
2020-11-23 08:24:07 +0700

    link image to public

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(flash);
 
